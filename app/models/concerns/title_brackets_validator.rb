6395c5636e7ee0f887adb4ec1b115cf58d9a0fbd
jjkazm <jjkazm@gmail.com>
2019-03-06 21:16:00 +0100

    Check if title contains empty brackets

--- app/models/concerns/title_brackets_validator.rb
+++ app/models/concerns/title_brackets_validator.rb
@@ -1,7 +1,15 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
-
+    if contains_empty_brackets(record.title)
+     record.errors.add :title, 'Title contains empty brackets'
+   end
   end
 
+private
+
+  #checks if there are empty brackets (including brackets with only spaces)
+  def contains_empty_brackets(str)
+    %w( {} () [] ).any? { |x| str.gsub(" ","").include? x }
+  end
 
 end

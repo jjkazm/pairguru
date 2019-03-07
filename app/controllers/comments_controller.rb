729fd4dd047d5f2e4f0cc506c4882704fe5d79c6
jjkazm <jjkazm@gmail.com>
2019-03-07 22:39:36 +0100

    Create comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @comment = @movie.comments.new(comment_params)
+    if @comment.save
+      flash[:success] = "Comment has been added"
+      redirect_to movie_path(@movie)
+    else
+      flash[:danger] = "#{@comment.errors.full_messages.first}"
+      redirect_to movie_path(@movie)
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end

a02943b0d7a6a5f79bd244925ff08818b5525434
jjkazm <jjkazm@gmail.com>
2019-03-07 22:28:22 +0100

    Create comment-movie association and test it

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_211313) do
+ActiveRecord::Schema.define(version: 2019_03_07_212035) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "movie_id"
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   it { should belong_to(:user) }
+  it { should belong_to(:movie) }
 end

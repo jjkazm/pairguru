ff66e2d072da20dbddb92c84023e9cc6faee5146
jjkazm <jjkazm@gmail.com>
2019-03-09 00:40:15 +0100

    Change comment body type from string to text

---
+++ db/migrate/20190308225754_change_body_to_be_text_in_comment.rb
@@ -0,0 +1,5 @@
+class ChangeBodyToBeTextInComment < ActiveRecord::Migration[5.2]
+  def change
+      change_column :comments, :body, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_07_212035) do
+ActiveRecord::Schema.define(version: 2019_03_08_225754) do
 
   create_table "comments", force: :cascade do |t|
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"

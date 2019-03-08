e03946f15b7b7e3eb1bee471b264162760932500
jjkazm <jjkazm@gmail.com>
2019-03-08 18:59:10 +0100

    Annotate models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#  movie_id   :integer
+#
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :movie
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  released_at :datetime
+#  avatar      :string
+#  genre_id    :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do

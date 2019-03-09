0b621826bf2d4105710375ad2c249fe1fad2c595
jjkazm <jjkazm@gmail.com>
2019-03-09 10:57:17 +0100

    Test comment validations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@
 # Table name: comments
 #
 #  id         :integer          not null, primary key
-#  body       :string
+#  body       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  user_id    :integer
@@ -15,4 +15,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:movie) }
+  it { should validate_presence_of(:body)}
+  it { should validate_length_of(:body).is_at_least(5).on(:create)}
+  it { should validate_length_of(:body).is_at_most(250).on(:create)}  
 end

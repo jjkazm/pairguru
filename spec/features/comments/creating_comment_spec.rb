7e5da845b488f9a38ec7bb9cccea02fd3d47b405
jjkazm <jjkazm@gmail.com>
2019-03-10 15:31:14 +0100

    Add tests for creating and deleting comments

--- spec/features/comments/creating_comment_spec.rb
+++ spec/features/comments/creating_comment_spec.rb
@@ -1,21 +1,46 @@
 require 'rails_helper'
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
 
 RSpec.feature "Adding comments" do
   before do
+      DatabaseCleaner.clean
       @movie = create(:movie)
       @user = create(:user)
       @user.confirm
   end
 
-  scenario "logged user adds comment" do
+  scenario "works for logged user" do
     sign_in @user
-    
+
     visit movie_path(@movie)
 
     fill_in 'comment_body', with: "This is great"
 
     click_button "Create Comment"
     expect(page).to have_content("This is great")
+  end
 
+  scenario "can't be done for not logged user" do
+    visit movie_path(@movie)
+    expect(page).not_to have_button("Create Comment")
+  end
+
+  scenario "can't be done for user who already commented" do
+    create(:comment, user: @user, movie: @movie)
+    sign_in @user
+
+    visit movie_path(@movie)
+    expect(page).not_to have_button("Create Comment")
+  end
+
+  scenario "works for user who already commented, but deleted the comment" do
+    create(:comment, user: @user, movie: @movie)
+    sign_in @user
+
+    visit movie_path(@movie)
+    expect(page).not_to have_button("Create Comment")
+    click_link("Delete my comment")
+    expect(page).to have_button("Create Comment")
   end
 end
